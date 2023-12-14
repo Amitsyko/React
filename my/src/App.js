f52e6b9a410036539eb5a7c37ab8a7cc4adafbb1
Amit <vishamit8827@gmail.com>
2023-12-14 17:50:21 +0530

    React Router Dom

--- my/src/App.js
+++ my/src/App.js
@@ -5,41 +5,55 @@ import TextForm from './components/TextForm';
 import About from './components/About';
 import React, { useState } from 'react';
 import Alert from './components/Alert';
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState('light'); //Wheather dark Mode is enable or not.
   const [alert, setAlert] = useState(null);
 
-  const showAlert = (message,type) =>{
+  const showAlert = (message, type) => {
     setAlert({
       msg: message,
       type: type
-    })  
+    })
     setTimeout(() => {
       setAlert(null);
     }, 1500);
   }
-  const toggleMode = () =>{
-    if(mode==='light'){
+  const toggleMode = () => {
+    if (mode === 'light') {
       setMode('dark')
-      document.body.style.backgroundColor ="grey";
-      showAlert("Dark Mode has been Enabled","success")
-    }else{
+      document.body.style.backgroundColor = "grey";
+      showAlert("Dark Mode has been Enabled", "success")
+    } else {
       setMode('light')
-      document.body.style.backgroundColor ="white";
-      showAlert("Light Mode has been Enabled","success")
+      document.body.style.backgroundColor = "white";
+      showAlert("Light Mode has been Enabled", "success")
     }
   }
   return (
-  <>  
-    {/* <Navbar/> */}
-    <Navbar  title="TextUlties" about="About us" mode={mode} toggleMode={toggleMode}/>
-    <center><Alert alert={alert}/></center>
-    <TextForm head="This is Text Counter Form" sub="Fill this Form :-" mode={mode} showAlert={showAlert}/>   
-    <About/>
-  </>
-   
+    <>
+      {/* <Navbar/> */}
+      <Router>
+        <Navbar title="TextUlties" about="About us" mode={mode} toggleMode={toggleMode} />
+        <center><Alert alert={alert} /></center>
+
+        <Routes>
+          <Route path="/about" element={<About about="About us" />}>
+          </Route>
+
+          <Route path="/" element={<TextForm head="This is Text Counter Form" sub="Fill this Form :-" mode={mode} showAlert={showAlert} />}>
+
+          </Route>
+        </Routes>
+      </Router>
+    </>
+
   );
 }
 
