a6c38ef88d92d762562a415abfd928bec99c17bd
Amit <vishamit8827@gmail.com>
2024-02-12 19:26:35 +0530

    React.js

---
+++ newsapp/src/components/Newitem.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+export default class Newitem extends Component {
+  render() {
+    let {title,description,imageUrl} = this.props;
+    return (
+      <div>
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-4">
+              <div className="card" style={{width: "18rem"}}>
+                <img src={imageUrl} className="card-img-top" alt="..."/>
+                  <div className="card-body">
+                    <h5 className="card-title">{title}</h5>
+                    <p className="card-text">{description}</p>
+                    <a href="/" className="btn btn-primary">Read More</a>
+                  </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
