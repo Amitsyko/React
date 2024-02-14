ed5f8b0cc1d7e86ce140c5d5680a7b6e7b6b18c0
Amit <vishamit8827@gmail.com>
2024-02-14 16:36:03 +0530

    React.js

--- newsapp/src/components/News.js
+++ newsapp/src/components/News.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Newitem from './Newitem'
 import Spinner from './Spinner';
 import PropTypes from 'prop-types'
+// import News from './components/News';
 
 
 export default class News extends Component {
@@ -20,7 +21,7 @@ export default class News extends Component {
 
     constructor() {
         super();
-        console.log("Hello i am constructor");
+        // console.log("Hello i am constructor");
         this.state = {
             articles: [],
             loading: false,
@@ -30,12 +31,12 @@ export default class News extends Component {
 
     //Display content API with this method :----
     async componentDidMount() {
-        console.log("CDM")
+        // console.log("CDM")
         let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=a924b902923447fdb68207df99a8f2fc&page=1&pageSize=${this.props.pageSize}`;
         this.setState({ loading: true });
         let data = await fetch(url);
         let parsedData = await data.json();
-        console.log(parsedData);
+        // console.log(parsedData);
         this.setState({
             articles: parsedData.articles,
             totalResults: parsedData.totalResults,
@@ -51,7 +52,7 @@ export default class News extends Component {
         this.setState({ loading: true });
         let data = await fetch(url);
         let parsedData = await data.json();
-        console.log(parsedData);
+        // console.log(parsedData);
         this.setState({
             page: this.state.page - 1,
             articles: parsedData.articles,
@@ -63,12 +64,12 @@ export default class News extends Component {
         console.log("Next");
 
         if (!(this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize))) {
-            console.log("CDM")
+            // console.log("CDM")
             let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=a924b902923447fdb68207df99a8f2fc&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
             this.setState({ loading: true });
             let data = await fetch(url);
             let parsedData = await data.json();
-            console.log(parsedData);
+            // console.log(parsedData);
             this.setState({
                 page: this.state.page + 1,
                 articles: parsedData.articles,
@@ -79,7 +80,7 @@ export default class News extends Component {
 
 
     render() {
-        console.log("Render")
+        // console.log("Render")
         return (
             <div>
                 <div className="container my-3">
